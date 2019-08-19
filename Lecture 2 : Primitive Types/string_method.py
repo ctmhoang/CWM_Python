be9a0c29b62829153b2d8609ad89749432bb38fb
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-08-19 06:36:25 -0700

    Create string_method.py

---
+++ Lecture 2 : Primitive Types/string_method.py
@@ -0,0 +1,17 @@
+course = "  python programming  "
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip()) #rstrip lstrip
+print(course.find("Pro") // .index("Pro") give value error
+print(course.replace('p','j'))
+print('pro' in course)
+print('pro' not in course)
+
+# Some string method learnt in 6.00.1x
+#str.capitalize()
+#str.swapcase
+#str.isupper/lower
+#str.count
+#str.split
+#More information in https://docs.python.org/3/library/stdtypes.html#string-methods
