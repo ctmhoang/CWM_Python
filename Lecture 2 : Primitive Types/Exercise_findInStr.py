eda6fcfb84f1cec7f3edc867e07abea8504c618b
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-08-19 07:14:41 -0700

    Create Exercise_findInStr.py

---
+++ Lecture 2 : Primitive Types/Exercise_findInStr.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# exercises https://pythonbasics.org/exercises/
+
+# Find out if string find is case sensitive
+s = "That I ever did see. Dusty as the handle on the door"
+
+index = s.find("Dusty")
+print(index)
+
+if "Dusty" in s:
+    print("query found")
+
+# What if a query string appears twice in the string?
+
+
+# Write a program that asks console input and searches for a query.
