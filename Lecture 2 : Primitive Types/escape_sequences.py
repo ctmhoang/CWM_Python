2a936ecd1c492e6e5a08903f81e34187fd6b38bf
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-08-19 06:22:50 -0700

    Create  escape_sequences.py

---
+++ Lecture 2 : Primitive Types/escape_sequences.py
@@ -0,0 +1,16 @@
+# \\
+# '\
+# "\
+# \n Carriage return
+
+message = """
+Python
+Programming
+"""
+print(message)
+
+message2 = "\n a new line \n"
+print(message2)
+
+message3 = "Single quote \' "
+print(message3)
