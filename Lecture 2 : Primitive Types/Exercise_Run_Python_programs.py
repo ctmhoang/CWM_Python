4c74c58f00a36694d03970f084d7b7e0ed7339df
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-08-19 06:46:54 -0700

    Create Exercise_Run_Python_programs.py

---
+++ Lecture 2 : Primitive Types/Exercise_Run_Python_programs.py
@@ -0,0 +1,51 @@
+# All exercise found in:  https://pythonbasics.org/exercises/
+# Run Python programs
+
+# Make a Python program that prints your name.
+print("Cameron")
+
+# Make a program that displays the lyrics of a song.
+she_loves_you = """
+She loves you, yeah, yeah, yeah
+She loves you, yeah, yeah, yeah
+She loves you, yeah, yeah, yeah, yeah
+
+You think you lost your love,
+When I saw her yesterday.
+It's you she's thinking of
+And she told me what to say.
+She says she loves you
+And you know that can't be bad.
+Yes, she loves you
+And you know you should be glad. Ooh!
+
+She said you were to know
+That she almost lost her mind.
+And now she says she knows
+You're not the hurting kind.
+She says she loves you
+And you know that can't be bad.
+Yes, she loves you
+And you know you should be glad. Ooh!
+
+She loves you, yeah, yeah, yeah
+She loves you, yeah, yeah, yeah
+And with a love like that
+You know you should be glad.
+
+And now it's up to you,
+I think it's only fair,
+Pride can hurt you, too,
+Apologize to her
+Because she loves you
+And you know that can't be bad.
+Yes, she loves you
+And you know you should be glad. Ooh!
+
+She loves you, yeah, yeah, yeah
+She loves you, yeah, yeah, yeah
+And with a love like that
+You know you should be glad. 
+"""
+
+print(she_loves_you)
