3e464d82dcec8a82bb9439b7b2c0ec59ce4f7abd
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-08-19 06:55:41 -0700

    Create Exercise_variables.py

---
+++ Lecture 2 : Primitive Types/Exercise_variables.py
@@ -0,0 +1,17 @@
+# https://pythonbasics.org/exercises/
+# Variables
+
+
+# Make a program that displays several numbers.
+for _ in range(1,4):
+  print(f"Var ({_}) is ({_}")
+
+# Make a program that solves and shows the summation of 64 + 32.
+
+print("Summation 1 is: ", 64 + 32 )
+
+# Do the same as in 2, but make it sum x + y.
+x = 64
+y = 32
+sum = x + y
+print("Summation 2 is: " ,sum)
