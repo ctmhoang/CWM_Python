6362bf7a97656077dcf9927440fcfa8f7b8fd19d
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-08-19 07:29:02 -0700

    Create Exercise_splitStr.py

---
+++ Lecture 2 : Primitive Types/Exercise_splitStr.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# exercises https://pythonbasics.org/exercises/
+
+
+s = "Its too easy"
+words = s.split()
+print(words)
+
+print(len(words))
+print(len(s))
+
+word = "Easy"
+x = list(word)
+print(x)
+
+
+# Can a string be split on multiple characters?
+Yup
+
+# Can you split a string this string?: World, Earth, America, Canada
+str = "World, Earth, America, Canada"
+str.split(,)
+
+# Given an article, can you split it based on phrases?
+Yup
