7b4f8e576810572c68e7b5345f14c38a33faa74f
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-08-29 20:36:35 -0700

    Create LectureCode_keyboardInput.py

---
+++ Lecture 3 : Control Flow/LectureCode_keyboardInput.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# https://codewithmosh.com/courses/423295/lectures/6781755
+
+age = input("Please enter your age: ")
+message = "Eligible" if int(age) >= 18 else "Not eligible"
+print(message)
