ca14c08f495bdcfc65ad8f2574cecc9641b63dab
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-09-30 23:04:10 -0700

    Update pathFile.py

--- Notes/pathFile.py
+++ Notes/pathFile.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-Path(r"C:\Program Files\Microsoft")
+#Path(r"C:\Program Files\Microsoft")
 path = Path("ecommerce/__init__.py")
 path.exists()
 path.is_file()
@@ -19,3 +19,9 @@ print(path.absolute())
 #path.rmdir
 #path.rename("New name")
 #path.iterdir() #return a generator object (all of file and directory)
+
+py_files = [p for p in path.glob("*.py)]
+
+py_files = [p for p in path.glob("**/*.py)] #search recursive
+#or use 
+py_files = [p for p in path.rglob("*.py)] #search recursive
