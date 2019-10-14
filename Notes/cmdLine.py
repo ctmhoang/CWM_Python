2991d335e3f12cf4a03aef64676bcc3be78662d3
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-10-14 06:44:38 -0700

    Create cmdLine.py

---
+++ Notes/cmdLine.py
@@ -0,0 +1,8 @@
+import sys
+print(sys.argv)
+if(len(sys.argv) == 1:
+    print("USAGE: python3 app.py <passphrases>")
+else:
+    passphrases = sys.argv[1]
+    print("Passwords : ", passphrases)
+    
