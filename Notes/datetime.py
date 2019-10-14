07eeeb182c707cccfb0589c1922d82b1b23ad811
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-10-14 06:42:08 -0700

    Create datetime.py

---
+++ Notes/datetime.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+import time
+dt1 = datetime(2018,1,1)
+dt2 = datetime.now()
+dt = datetime.strptime("2018/01/01", "%Y/%m/%d")
+dt = datetime.fromtimestamp(time.time())
+print(f"{dt.year} / {dt.month}")
+print(dt.strftime("%Y/%m"))
+print(dt2 > dt1)
